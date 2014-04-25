1f71ca6f9ff47fb847cd8c7e22be6977d9d7f69c
Adrian Wobito <adrian.wobito@gmail.com>
2014-04-25 14:41:51 -0400

    Updates

---
+++ js/app-ck.js
@@ -0,0 +1,1 @@
+$(function(){$.stellar();$(".mainNav a").on("click",function(e){$(".nav li").removeClass("active");$(this).parent().addClass("active");$("body").scrollTo(this.hash,this.hash,{offset:-50});e.preventDefault()})});
--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 $(function(){
 	$.stellar();
+	$('.mainNav a').on('click',function(e){
+		$('.nav li').removeClass('active');
+		$(this).parent().addClass('active');
+		$('body').scrollTo(this.hash, this.hash,{offset:-50});
+		e.preventDefault();
+	});
 });
