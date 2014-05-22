d7de464f6d2f7d01cffe5b6b9df8080452b53b74
Adrian Wobito <adrian.wobito@gmail.com>
2014-05-22 14:50:19 -0400

    turned laravel

---
+++ app/views/_layouts/main.blade.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0">
+    <meta name="description" content="Adrian Wobito's personal portfolio for web development and design. As well as other multimedia based design. (Print, Video, Audio, Mobile)">
+    <meta name="author" content="Adrian Wobito">
+
+    <title>Adrian Wobito - Web Developer &amp; Designer</title>
+    <link href="css/style.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-1878302-4', 'wobito.ca');
+		ga('send', 'pageview');
+
+	</script>
+  </head>
+  <body id="home" data-spy="scroll" data-target="#bs-example-navbar-collapse-1">	
+		<header>
+			<nav class="navbar navbar-green navbar-fixed-top" role="navigation">
+				<div class="container">
+    				<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#home">Adrian Wobito</a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="mainNav nav navbar-nav">
+							<li class="active"><a href="#profile">Profile</a></li>
+							<li><a href="#resume">Resume</a></li>
+							<li><a href="#portfolio">Portfolio</a></li>
+							<li><a href="#contact">Contact</a></li>
+						</ul>
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="http://twitter.com/wobito" target="_blank" class="socials"><span class="fa fa-twitter"></span></a></li>
+							<li><a href="http://facebook.com/wobito" target="_blank" class="socials"><span class="fa fa-facebook"></span></a></li>
+							<li><a href="http://instagram.com/wobito" target="_blank" class="socials"><span class="fa fa-instagram"></span></a></li>
+							<li><a href="http://ca.linkedin.com/in/wobito/" target="_blank" class="socials"><span class="fa fa-linkedin"></span></a></li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>			
+		</header>
+		<section id="profile">
+			<div style="position:relative;">
+				<div class="container">
+					<h1 data-stellar-ratio="1.2">Adrian Wobito</h1>
+					<h3 data-stellar-ratio="1.4">Web Developer &amp; Designer</h3>
+
+					<div class="profile-box hidden-xs" data-stellar-ratio="1.4">						
+						<p>Email : <a href="mailto:adrian@wobito.ca">adrian@wobito.ca</a></p>
+						<p>Phone : +1-647-890-4638</p>					
+						<p>Twitter : <a href="http://twitter.com/wobito">@wobito</a></p>					
+					</div>
+				</div>
+			</div>		
+		</section>
+		
+		<section class="text-box" id="resume">
+			<div class="container">
+				<h1>What I Do <span class="small">| resume</span></h1>
+				<hr>
+				<div class="row">
+					<div class="col-sm-4">
+						<h3>Web Design</h3>
+						I design modern, grid based websites with user friendly layout and highly effective landing pages according to the latest usability principles.
+					</div>
+					<div class="col-sm-4">
+						<h3>Site Development</h3>
+						I use the latest html5/css3/jquery/php5/mysql technologies to built fully responsive, mobile friendly webpages with cross-browser compatibility.
+					</div>
+					<div class="col-sm-4">
+						<h3>Custom Development</h3>
+						I develop and configure custom web applications with "manage it yourself" interface providing domain and host services.
+					</div>
+				</div>				
+			</div>
+		</section>
+		<section class="stellar-bg" data-stellar-background-ratio="0.5">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<h3>Languages</h3>
+						<p>Advanced knowledge and training with XHTML, HTML5, CSS, AJAX, JavaScript, PHP, XML, SQL, Actionscript, Objective-C (iOS)</p>
+						<p>Advanced knowledge with MVC PHP Frameworks (Laravel, CakePHP, Codeigniter)</p>
+						<p>Advanced knowledge with PHP CMS (Wordpress, Joomla, Drupal)</p>
+					</div>
+					<div class="col-sm-4">
+						<h3>Software</h3>
+						<p>Trained In: Photoshop, Illustrator, InDesign, Flash, After Effects</p>
+						<p>Trained with Apple Pro Applications (FCP, Soundtrack, DVD Studio Pro)</p>
+						<p>Excellent knowledge with Mac OS X and Windows XP/Vista/7/8</p>
+					</div>
+					<div class="col-sm-4">
+						<h3>Servers &amp; Standards</h3>
+						<p>Training in (L|M|W)AMP Administration and Unix Server Administration</p>
+						<p>Advanced knowledge of Web Standards W3C and cross browser compatibility</p>
+					</div>
+				</div>
+				<p><a href="https://dl.dropboxusercontent.com/s/ct902tovbiww7sv/Adrian%20Wobito.docx?dl=1&token_hash=AAHP9DTGhD0V2lGzJnUf2hJaGByrdTbgLJ4phVQH2An6IQ" class="btn btn-green"><span class="fa fa-fw fa-download"></span> Download My Resume</a></p>
+			</div>
+
+		</section>
+		<section class="text-box" id="portfolio">
+			<div class="container">
+				<h1>What I Have Done So Far <span class="small">| portfolio</span></h1>
+				<hr>
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="port-item">
+							<img src="img/portfolio/coreChairWebsite.jpg" alt="" class="img-circle">	
+							<h4>CoreChair Website</h4>	
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="port-item">
+							<img src="img/portfolio/coreChairBanner.jpg" alt="" class="img-circle">	
+							<h4>CoreChair Tradeshow Banner</h4>	
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="port-item">
+							<img src="img/portfolio/wobitoGems.jpg" alt="" class="img-circle">
+							<h4>Wobito Gems Website</h4>	
+						</div>
+					</div>					
+				</div>
+				<br>
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="port-item">
+							<img src="img/portfolio/wobitoGemSnowflakeVideo.jpg" alt="" class="img-circle">
+							<h4>Wobito Snowflake Video</h4>								
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="port-item">
+							<img src="img/portfolio/wobitoGemSnowflakeVideo2.jpg" alt="" class="img-circle">
+							<h4>TVSN Wobito Snowflake Video</h4>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="port-item">
+							<img src="img/portfolio/cpRegistrationApp.jpg" alt="" class="img-circle">
+							<h4>Event Registration App (iOS)</h4>	
+						</div>
+					</div>					
+				</div>
+				<br>
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="port-item">
+							<img src="img/portfolio/wssc.jpg" alt="" class="img-circle">
+							<h4>WSSC Website</h4>							
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="port-item">
+							<img src="img/portfolio/craigProctor.jpg" alt="" class="img-circle">
+							<h4>Craig Proctor Website</h4>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="port-item">
+							<img src="img/portfolio/onlineQuantumLeap.jpg" alt="" class="img-circle">
+							<h4>Online Quantum Leap Portal</h4>
+						</div>
+					</div>
+				</div>
+				<br>
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="port-item">
+							<img src="img/portfolio/cpCoaching.jpg" alt="" class="img-circle">
+							<h4>Craig Proctor Coaching Portal</h4>					
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="port-item">
+							<img src="img/portfolio/cannounAirways.jpg" alt="" class="img-circle">
+							<h4>Cannoun Airways</h4>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="port-item">
+							<img src="img/portfolio/footballManager.jpg" alt="" class="img-circle">
+							<h4>Football Manager Website</h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="contact" class="stellar-bg" data-stellar-background-ratio="0.5">
+			<div class="container">
+				<h1>Am I available for hire? <span class="small">| contact me</span></h1>
+				<hr>
+				<div class="row">
+					<div class="col-sm-8">
+						<div class="form-group">
+							<label>Name:</label>
+							<input type="text" name="name" class="form-control">
+						</div>		
+						<div class="form-group">
+							<label>Email:</label>
+							<input type="text" name="email" class="form-control">
+						</div>		
+						<div class="form-group">
+							<label>Phone:</label>
+							<input type="text" name="phone" class="form-control">
+						</div>		
+						<div class="form-group">
+							<label>Message:</label>
+							<textarea name="message" class="form-control" rows="10"></textarea>
+						</div>		
+						<div class="form-group">
+							<button class="btn btn-green"><span class="fa fa-fw fa-envelope"></span> Send Message</button>
+						</div>
+
+					</div>
+					<div class="col-sm-4">
+						<p>Email : <a href="mailto:adrian@wobito.ca">adrian@wobito.ca</a></p>
+						<p>Phone : +1-647-890-4638</p>					
+						<p>Twitter : <a href="http://twitter.com/wobito">@wobito</a></p>
+					</div>
+				</div>				
+			</div>
+		</section>
+
+  	<!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
